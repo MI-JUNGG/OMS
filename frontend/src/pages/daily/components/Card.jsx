075ac6e9346d9e1cc946be5347253e48ea0a20e8
jyh2610 <jogg4177@naver.com>
2023-04-09 21:11:28 +0900

    ADD: 일정카드 레이아웃

--- frontend/src/pages/daily/components/Card.jsx
+++ frontend/src/pages/daily/components/Card.jsx
@@ -4,16 +4,21 @@ function Card() {
     return (
         <div>
             <div className="card">
-                <span>일정카드</span>
                 <div className="selectColor">
                     <span>color</span>
                     <span>color</span>
                     <span>color</span>
                 </div>
-            </div>
-            <div className="cardTitle">
-        <span>title</span>
-        <div className="cardTitleInput"></div>
+                <div className="cardTitle">
+                    <span>title</span>
+                    <div className="cardTitleInput"></div>
+                </div>
+                <div className="contents">
+                    <span>contents</span>
+                    <div className="contentsInput"></div>
+                </div>
+
+                <button type="button">완료</button>
             </div>
         </div>
     );
