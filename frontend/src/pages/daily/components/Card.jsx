aca07a72f6dd9b5dee6cf0ef78dc7817a789eb08
jyh2610 <jogg4177@naver.com>
2023-04-09 20:14:29 +0900

    ADD: cardComponents file

--- frontend/src/pages/daily/components/Card.jsx
+++ frontend/src/pages/daily/components/Card.jsx
@@ -1,9 +1,20 @@
-import "./Card.css";
+import "./Card.scss";
 
 function Card() {
     return (
         <div>
-            <div className="card">일정카드</div>
+            <div className="card">
+                <span>일정카드</span>
+                <div className="selectColor">
+                    <span>color</span>
+                    <span>color</span>
+                    <span>color</span>
+                </div>
+            </div>
+            <div className="cardTitle">
+        <span>title</span>
+        <div className="cardTitleInput"></div>
+            </div>
         </div>
     );
 }
