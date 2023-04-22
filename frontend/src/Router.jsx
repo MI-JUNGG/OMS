958283b04b351ea97a3634a064434c3b366dc91c
Jangchan0 <jchy16@gmail.com>
2023-04-22 21:54:35 +0900

    바보같은 실수

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Detail from "./pages/Detail";
 import Daily from "./pages/daily/Daily";
+import Nav from "./pages/Nav";
 
 function Router() {
     return (
         <BrowserRouter>
+            <Nav />
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/detail" element={<Detail />} />
