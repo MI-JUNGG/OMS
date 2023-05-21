f09bd981f5a681f23314ea694d151dc1281ec35f
yeyun <hallo.yunye@gmail.com>
2023-05-21 22:59:38 +0900

    wip

--- backend/src/controllers/getpageController.js
+++ backend/src/controllers/getpageController.js
@@ -3,13 +3,13 @@ const { catchAsync, detectError } = require("../utils/detectError");
 
 const monthPage = catchAsync(async (req, res) => {
   const { userId, startDate, endDate } = req.query;
-  console.log("cont", userId, typeof startDate);
+
   if (!userId || !startDate || !endDate)
     detectError("NEED_USER_ID OR NEED_DATE_INFO", 400);
 
   const result = await getpageService.monthPage(userId, startDate, endDate);
 
-  return res.status(200).json({ result: result });
+  return res.status(200).json(result);
 });
 
 module.exports = {
