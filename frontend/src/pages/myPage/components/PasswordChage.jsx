06babb19712db3a464e9b52900cebdc14ba61f10
Jangchan0 <jchy16@gmail.com>
2023-05-28 22:33:12 +0900

    FEAT: 비밀번호 변경 UI 추가

--- frontend/src/pages/myPage/components/PasswordChage.jsx
+++ frontend/src/pages/myPage/components/PasswordChage.jsx
@@ -1,7 +1,34 @@
 import "./PasswordChage.scss";
 
 function PasswordChage() {
-    return <div>PasswordChage</div>;
+    return (
+        <div className="passwordChageContainer">
+            <div className="passwordChage">
+                <span>현재 비밀번호</span>
+                <input
+                    className="passwordInput"
+                    placeholder="비밀번호"
+                    type="password"
+                />
+            </div>
+            <div className="passwordChage">
+                <span>새 비밀번호</span>
+                <input
+                    className="passwordInput"
+                    placeholder="비밀번호"
+                    type="password"
+                />
+            </div>
+            <div className="passwordChage">
+                <span>새 비밀번호 확인</span>
+                <input
+                    className="passwordInput"
+                    placeholder="비밀번호"
+                    type="password"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default PasswordChage;
