bab0e5d1a0f00b7ec35801b3f9f7ad89270df72d
yeyun <hallo.yunye@gmail.com>
2023-06-04 17:22:09 +0900

    [ADD] monthPage 조회

--- backend/src/models/getpageDao.js
+++ backend/src/models/getpageDao.js
@@ -1,18 +1,6 @@
 const { appDataSource } = require("./appDataSource");
 
-// const formatDateTime = (date) => {
-//   const year = date.getFullYear();
-//   const month = String(date.getMonth() + 1).padStart(2, "0");
-//   const day = String(date.getDate()).padStart(2, "0");
-//   const hours = String(date.getHours()).padStart(2, "0");
-//   const minutes = String(date.getMinutes()).padStart(2, "0");
-//   const seconds = String(date.getSeconds()).padStart(2, "0");
-
-//   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-// };
-
 const monthPage = async (userId, startMonth, endMonth) => {
-  console.log("dao", startMonth, endMonth);
   const result = await appDataSource.query(
     `
     SELECT
@@ -27,17 +15,15 @@ const monthPage = async (userId, startMonth, endMonth) => {
     FROM
       card c
     WHERE c.user_id = ?
-    AND DATE_FORMAT(c.start_date, '%Y-%m') = ?
-    AND DATE_FORMAT(c.end_date, '%Y-%m') = ?
+    AND DATE_FORMAT(c.start_date, '%Y-%c') = ?
+    AND DATE_FORMAT(c.end_date, '%Y-%c') = ?
+    ORDER BY c.start_date ASC
     `,
-    [userId, `%${startMonth}%`, `%${endMonth}%`]
+    [userId, startMonth, endMonth]
   );
-  console.log(result);
+
   return result;
 };
-
-// AND c.start_date LIKE CONCAT(?, '%')
-// AND c.end_date LIKE CONCAT(?, '%')
 
 module.exports = {
   monthPage,
