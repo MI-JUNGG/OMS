03f513af156aedcadbf9b1b17fa6bba6ebe77c54
jyh2610 <jogg4177@naver.com>
2023-06-07 21:59:08 +0900

    ADD:일정 렌더링

--- frontend/src/pages/daily/components/Seletime.jsx
+++ frontend/src/pages/daily/components/Seletime.jsx
@@ -2,6 +2,34 @@ import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./Selectime.scss";
 
+const hours = [
+    "00:00",
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+    "23:00",
+    "24:00",
+];
+
 function Seletime() {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
@@ -9,9 +37,9 @@ function Seletime() {
     const [date, setDate] = useState(day);
     const navigate = useNavigate();
     const [test, setTest] = useState({
-        start: "2023-6-8 01:00",
-        end: "2023-6-8 20:00",
-        title: "test title",
+        start: "2023-06-08 01:00",
+        end: "2023-06-08 5:00",
+        title: "Test Title",
         color: "yellow",
     });
     const { start, end, title, color } = test;
@@ -48,6 +76,8 @@ function Seletime() {
         setDate(formattedDate);
     }, [location]);
 
+    let isTitleRendered = false;
+
     return (
         <div className="dayTable">
             <div className="dayChanger">
@@ -59,7 +89,7 @@ function Seletime() {
                     +
                 </div>
             </div>
-            {hours.map((hour) => {
+            {hours.map((hour, index) => {
                 const startTime = new Date(start).getHours();
                 const endTime = new Date(end).getHours();
                 const hourSplit = hour.split(":");
@@ -74,9 +104,22 @@ function Seletime() {
                     <div key={hour} className="timeContainer">
                         <div className="timeSlot">{hour}</div>
                         <div className="timeBorder"></div>
-                        <div className="contents" style={{ backgroundColor }}>
-                            {hourValue === startTime && title}
-                        </div>
+                        {hourValue >= getStartTime &&
+                        hourValue <= getEndTime ? (
+                            <div
+                                className={`otherContents ${
+                                    hourValue === getStartTime && `first`
+                                }`}
+                                style={{ backgroundColor }}
+                            >
+                                {hourValue === getStartTime &&
+                                    !isTitleRendered && (
+                                        <div className="title">{title}</div>
+                                    )}
+                            </div>
+                        ) : (
+                            <div className="contents"></div>
+                        )}
                     </div>
                 );
             })}
@@ -85,31 +128,3 @@ function Seletime() {
 }
 
 export default Seletime;
-
-export const hours = [
-    "00:00",
-    "01:00",
-    "02:00",
-    "03:00",
-    "04:00",
-    "05:00",
-    "06:00",
-    "07:00",
-    "08:00",
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-    "23:00",
-    "24:00",
-];
