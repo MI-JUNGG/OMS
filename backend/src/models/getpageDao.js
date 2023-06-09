a194fd6c1a8feac04b014a6e1f5a6f12140b359c
yeyun <hallo.yunye@gmail.com>
2023-06-09 23:37:37 +0900

    [MODIFY] month 페이지 조회 및 커스텀 팔레트 조회 기능 구현

--- backend/src/models/getpageDao.js
+++ backend/src/models/getpageDao.js
@@ -1,87 +1,48 @@
 const { appDataSource } = require("./appDataSource");
 
-// const monthPage = async (userId, startMonth, endMonth) => {
-//   const result = await appDataSource.query(
-//     `
-//     SELECT
-//       c.id                        AS cardId,
-//       c.title                     AS title,
-//       c.repeat_id                 AS \`repeat\`,
-//       c.start_date                AS \`start\`,
-//       c.end_date                  AS end,
-//       c.deadline                  AS deadline,
-//       c.memo                      AS memo,
-//       c.link                      AS link,
-//       m.color_palette_id          AS colorPaletteId
-//     FROM
-//       card c
-//     LEFT JOIN mypage m            ON c.user_id = m.user_id
-//     WHERE c.user_id = ?
-//     AND DATE_FORMAT(c.start_date, '%Y-%m') = ?
-//     AND DATE_FORMAT(c.end_date, '%Y-%m') = ?
-//     ORDER BY c.start_date ASC
-//     `,
-//     [userId, startMonth, endMonth]
-//   );
-
-//   return result;
-// };
-
 const monthPage = async (userId, startMonth, endMonth) => {
-  const queryRunner = appDataSource.createQueryRunner();
+  const palette = await appDataSource.query(
+    `
+  SELECT
+    m.color_palette_id          AS colorPaletteId,
+    m.color1                    AS color1,
+    m.color2                    AS color2,
+    m.color3                    AS color3,
+    m.color4                    AS color4,
+    m.color5                    AS color5,
+    m.color6                    AS color6,
+    m.color7                    AS color7
+  FROM
+    mypage m
+  WHERE
+    m.user_id = ?
+  `,
+    [userId]
+  );
 
-  await queryRunner.connect();
-  await queryRunner.startTransaction();
+  const monthCard = await appDataSource.query(
+    `
+    SELECT
+      c.id                        AS cardId,
+      c.title                     AS title,
+      c.repeat_id                 AS \`repeat\`,
+      c.start_date                AS \`start\`,
+      c.end_date                  AS end,
+      c.deadline                  AS deadline,
+      c.memo                      AS memo,
+      c.link                      AS link
+    FROM
+      card c
+    LEFT JOIN mypage m            ON c.user_id = m.user_id
+    WHERE c.user_id = ?
+    AND DATE_FORMAT(c.start_date, '%Y-%m') = ?
+    AND DATE_FORMAT(c.end_date, '%Y-%m') = ?
+    ORDER BY c.start_date ASC
+    `,
+    [userId, startMonth, endMonth]
+  );
 
-  try {
-    const result1 = await queryRunner.query(
-      `
-      SELECT
-        color1,
-        color2,
-        color3,
-        color4,
-        color5,
-        color6,
-        color7
-      FROM
-        color_palette
-      WHERE id = 6 
-      `
-    );
-
-    const result2 = await queryRunner.query(
-      `
-      SELECT
-        c.id                        AS cardId,
-        c.title                     AS title,
-        c.repeat_id                 AS \`repeat\`,
-        c.start_date                AS \`start\`,
-        c.end_date                  AS end,
-        c.deadline                  AS deadline,
-        c.memo                      AS memo,
-        c.link                      AS link,
-        m.color_palette_id          AS colorPaletteId
-      FROM
-        card c
-      LEFT JOIN mypage m            ON c.user_id = m.user_id 
-      WHERE c.user_id = ?
-      AND DATE_FORMAT(c.start_date, '%Y-%m') = ?
-      AND DATE_FORMAT(c.end_date, '%Y-%m') = ?
-      ORDER BY c.start_date ASC
-      `,
-      [userId, startMonth, endMonth]
-    );
-
-    await queryRunner.commitTransaction();
-
-    return [result1, result2];
-  } catch (err) {
-    console.log(err);
-    await queryRunner.rollbackTransaction();
-  } finally {
-    await queryRunner.release();
-  }
+  return { palette, monthCard };
 };
 
 const weekPage = async (userId, startMonth, endMonth) => {
