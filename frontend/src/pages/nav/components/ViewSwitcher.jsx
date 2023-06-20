cce83cd5f77ebf5e06f83acc649a764b8b0d5540
Jangchan0 <jchy16@gmail.com>
2023-06-20 23:08:28 +0900

    FEAT: weekPage 쿼리문 추가

--- frontend/src/pages/nav/components/ViewSwitcher.jsx
+++ frontend/src/pages/nav/components/ViewSwitcher.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { view } from "../../../modules/module/viewSelector";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./ViewSwitcher.scss";
 
 function ViewSwitcher() {
@@ -10,6 +10,27 @@ function ViewSwitcher() {
     const changeView = (value) => {
         dispatch(view(value));
     };
+
+    const date = new Date();
+    const dayOfWeek = date.getDay();
+    const sundayDate = new Date(date);
+    sundayDate.setDate(date.getDate() - dayOfWeek);
+
+    // sundayDate 값을 YYYY-MM-DD 형식의 날짜 문자열로 변환
+    const formattedSundayDate = `${sundayDate.getFullYear()}-${
+        sundayDate.getMonth() + 1
+    }-${sundayDate.getDate()}`;
+
+    // useLocation 훅을 사용하여 현재 경로와 쿼리 파라미터를 가져옴
+    const location = useLocation();
+    const { pathname, search } = location;
+
+    // sundayDate를 URL 쿼리 파라미터로 추가
+    const updatedSearch = new URLSearchParams(search);
+    updatedSearch.set("sundayDate", formattedSundayDate);
+
+    // 쿼리 파라미터가 포함된 URL을 만듦
+    const updatedUrl = `${pathname}?${updatedSearch.toString()}`;
 
     return (
         <>
@@ -24,7 +45,10 @@ function ViewSwitcher() {
                             onClick={() => changeView(value)}
                         >
                             <Link
-                                to={form.element[i]}
+                                to={{
+                                    pathname: form.element[i],
+                                    search: updatedSearch.toString(),
+                                }}
                                 style={{
                                     textDecoration: "none",
                                 }}
@@ -40,13 +64,3 @@ function ViewSwitcher() {
 }
 
 export default ViewSwitcher;
-
-// <div className="monthView">
-//                     <Link to="/"> Month</Link>
-//                 </div>
-//                 <div className="weekView">
-//                     <Link to="/weekly"> Week</Link>
-//                 </div>
-//                 <div className="dayView">
-//                     <Link to="/daliy">Day</Link>
-//                 </div>
